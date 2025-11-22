7c12c99494b58fce026479fe05b3ac0e2876b2ca
weldisep <weldhisep.com>
2025-11-22 13:07:02 +0100

    wth

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 # src/main.py
 
 import argparse
-from src.models.model1_deterministic import build_and_solve_model1
-from src.models.model2_multi_period import build_and_solve_model2
-from src.models.model3_stochastic import build_and_solve_model3
+from models.model1_deterministic import build_and_solve_model1
+from models.model2_multi_period import build_and_solve_model2
+from models.model3_stochastic import build_and_solve_model3
 
 def main():
     parser = argparse.ArgumentParser()
